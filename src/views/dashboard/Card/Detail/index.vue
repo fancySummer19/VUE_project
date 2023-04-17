e2382aba49eaa4771728de2a586fc157013cd387
fancySummer19 <2458290516@qq.com>
2023-04-17 14:54:08 +0800

    master

---
+++ src/views/dashboard/Card/Detail/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="card-header">
+        <span>{{title}}</span>
+        <i class="el-icon-info"></i>
+    </div>
+    <div class="card-content">12306</div>
+    <div class="card-charts">
+        <slot name="cahrts">
+        </slot>
+    </div>
+    <div class="card-footer">
+        <slot name="footer">
+
+        </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Detail',
+    props:['title']
+}
+</script>
+
+<style>
+    .card-header {
+        display: flex;
+        justify-content: space-between;
+        color: silver;
+    }
+    .card-content {
+        font-size: 30px;
+        padding: 10px 0;
+    }
+    .card-charts {
+        height: 50px;
+    }
+    .card-footer {
+        border-top: 1px solid #eee;
+    }
+</style>
---
+++ src/views/dashboard/Card/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <el-card>
+          <Detail :title="销售总额"/>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail :title="访问量"/>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail :title="支付笔数"/>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail :title="运营活动效果"/>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Detail from "./Detail";
+export default {
+  name: "Cart",
+  components: {
+    Detail,
+  },
+};
+</script>
+
+<style>
+</style>
